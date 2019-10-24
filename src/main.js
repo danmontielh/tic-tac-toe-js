8e3abac833812484a30b8f7fae662666f8681828
Dan Montiel <danmontielh@gmail.com>
2019-10-24 18:23:52 -0500

    game logic fixed

--- src/main.js
+++ src/main.js
@@ -1,107 +1,100 @@
-// Board 
+// Board
 const grid = () => {
-    return Array.from(document.getElementsByClassName('t'));
+  return Array.from(document.getElementsByClassName("t"));
 };
 
-
-
 const board = () => {
+  const getId = el => {
+    return Number.parseInt(el.id[1]);
+  };
 
-   
-
-    const getId = (el) => {
-        return Number.parseInt(el.id[1]);
-    };
-
-    return { getId };
-
+  return { getId };
 };
 
 const createPlayer = (mark, name) => {
-    return {mark, name};
-}
-
-
-
-const gameEngine = () => {
-
-    const winningCombos = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 4, 8],
-        [2, 4, 6],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8]
-      ];
-
-      const getElementsAll = (arr) => arr.forEach((el) => {return el.innerText });
-
-
-      const winGame = () => {
-          let victory = false;
-            winningCombos.forEach((combo) => {
-                const _grid = grid();
-                const getClassCombo = [_grid[combo[0]],_grid[combo[1]], _grid[combo[2]] ];
-                if (getElementsAll(getClassCombo)) {
-                    victory = true;
-                }
-            });
-      };
-
-
-    const playerTurn = (index, markPlayer) => {
-        return grid()[index].innerText = markPlayer;
-    };
-
-    return { playerTurn, winGame};
-
-
+  return { mark, name };
 };
 
+const gameEngine = () => {
+  const winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8]
+  ];
+
+  const getElementsAll = arr =>
+    arr.every(el => el.innerText === arr[0].innerText && el.innerText !== "");
 
 
 
+  const endGame = comboWin => {
+    comboWin.forEach(el => {
+      el.classList.add("winner");
+    });
+  };
+
+  const winGame = disable => {
+    let victory = false;
+    winningCombos.forEach(combo => {
+      const _grid = grid();
+      const getClassCombo = [_grid[combo[0]], _grid[combo[1]], _grid[combo[2]]];
+      if (getElementsAll(getClassCombo)) {
+        victory = true;
+        endGame(getClassCombo);
+        disable();
+      }
+    });
+  };
+
+  const playerTurn = (index, markPlayer) => {
+      if (grid()[index].innerText == "") {
+        (grid()[index].innerText = markPlayer); 
+      } else {
+           alert('Please, this is an invalid move');
+      }
+  };
+
+  return { playerTurn, winGame };
+};
 
 const runGame = () => {
+  let turn = true;
+  const newGame = board();
+  const game = gameEngine();
+  let playerOne = createPlayer("x", "Player One");
+  let playerTwo = createPlayer("o", "Player Two");
 
-    
-
-    let turn = true;
-    const newGame = board();
-    const game = gameEngine();
-    let playerOne = createPlayer('x', 'Player One');
-    let playerTwo = createPlayer('o', 'Player Two');
-
-
-    
-
-    const playerTurns = ($event) => {
-        if (!game.winGame()) {
-            if (turn == true) {
-                game.playerTurn(newGame.getId($event.target), playerOne.mark );
-                turn = false;    
-    
-            }else {
-                game.playerTurn(newGame.getId($event.target), playerTwo.mark);
-                turn = true;
-            }
-        }
+  const playerTurns = $event => {
+    if (turn == true) {
+      game.playerTurn(newGame.getId($event.target), playerOne.mark);
+      turn = false;
+      if (!game.winGame(PlayerEnd)) {
+        console.log(playerOne.name);
+      }
+    } else {
+      game.playerTurn(newGame.getId($event.target), playerTwo.mark);
+      turn = true;
+      if (!game.winGame(PlayerEnd)) {
+      }
     }
+  };
 
+  const playerOneListen = () => {
+    grid().forEach(el => el.addEventListener("click", playerTurns));
+  };
 
+  const PlayerEnd = () => {
+    grid().forEach(el => el.removeEventListener("click", playerTurns));
+  };
 
-    const playerOneListen = () => {
-        grid().forEach(el => el.addEventListener('click', playerTurns) );
-    };
-
-
-    return{ playerOneListen  };
+  return { playerOneListen };
 };
 
 let launch = runGame();
 
 launch.playerOneListen();
-
-
