49c99b3c90108ff69b6a2e58bfebf5de0b3e025c
Dan Montiel <danmontielh@gmail.com>
2019-10-24 15:36:11 -0500

    fix changes

--- src/main.js
+++ src/main.js
@@ -21,42 +21,4 @@ const createPlayer = (mark, name) => {
 }
 
 
-const game = () => {
-    let turn = true;
-    const newGame = board();
-    let playerOne = createPlayer('x', 'Player One');
-    let playerTwo = createPlayer('o', 'Player Two');
-
-
-    const playerTurn = (index, markPlayer) => {
-        return newGame.grid()[index].innerText = markPlayer;
-    };
-
-
-
-
-
-    const playerTurns = ($event) => {
-        if (turn == true) {
-            playerTurn(newGame.getId($event.target), playerOne.mark);
-            turn = false;
-        }else {
-            playerTurn(newGame.getId($event.target), playerTwo.mark);
-            turn = true;
-        }
-    }
-
-
-
-    const playerOneListen = () => {
-        newGame.grid().forEach(el => el.addEventListener('click', playerTurns) );
-    };
-
-
-    return{ playerOneListen  };
-};
-
-let launch = game();
-
-launch.playerOneListen();
 
